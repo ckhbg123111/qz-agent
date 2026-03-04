819da5886534d40bab367455e7877a891d0e9ebd
Kehao Chen <3170105539@zju.edu.cn>
2026-03-04 16:10:13 +0800

    修改推送Code

--- qz-agent-web/src/main/java/com/zhongjia/web/push/PushTaskConstants.java
+++ qz-agent-web/src/main/java/com/zhongjia/web/push/PushTaskConstants.java
@@ -5,11 +5,11 @@ public final class PushTaskConstants {
     private PushTaskConstants() {
     }
 
-    public static final String TAG_LAB_APPOINTMENT = "UUID_EXAMPLE_1";
-    public static final String TAG_REPORT = "UUID_EXAMPLE_3";
-    public static final String TAG_PRESCRIPTION = "UUID_EXAMPLE_2";
-    public static final String TAG_PRESCRIPTION_2 = "UUID_EXAMPLE_7";
-    public static final String TAG_FOLLOW_UP = "UUID_EXAMPLE_10";
+    public static final String TAG_LAB_APPOINTMENT = "UUID_EXAMPLE_1";// 检验预约成功推送
+    public static final String TAG_REPORT = "UUID_EXAMPLE_9";// 检验出报告两天后推送推送
+    public static final String TAG_PRESCRIPTION = "UUID_EXAMPLE_6";// 四联处方开具推送
+    public static final String TAG_PRESCRIPTION_2 = "UUID_EXAMPLE_7"; // 二联处方开具推送
+    public static final String TAG_FOLLOW_UP = "UUID_EXAMPLE_10";// 处方开具14天后推送
 
     public static final String TASK_TYPE_REPORT_WARNING = "REPORT_WARNING";
     public static final String TASK_TYPE_FOLLOW_UP_REMINDER = "FOLLOW_UP_REMINDER";
